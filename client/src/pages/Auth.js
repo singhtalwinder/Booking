ebf83baa5fd6f937af6408587a43d760b30a8ac4
Talwinder Singh <singhtalwinder790@gmail.com>
2020-06-21 14:45:57 +0530

    Added Auth

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,8 +1,113 @@
 import React, { Component } from "react";
+import axios from "axios";
+import "./Auth.css";
 
 class AuthPage extends Component {
+	state = {
+		isLogin: true,
+	};
+
+	constructor(props) {
+		super(props);
+		this.emailEl = React.createRef();
+		this.passwordEl = React.createRef();
+	}
+
+	switchModeHandler = () => {
+		this.setState((prevState) => {
+			return { isLogin: !prevState.isLogin };
+		});
+	};
+
+	submitHandler = async (event) => {
+		event.preventDefault();
+		const email = this.emailEl.current.value;
+		const password = this.passwordEl.current.value;
+
+		if (this.state.isLogin) {
+			//signin
+			const query = `
+				query {
+					signin(email: "${email}", password: "${password}") {
+						...on AuthData {
+									userId
+									token
+									tokenExpiration
+							}
+							... on Error {
+								message
+						}
+					  }
+				}
+			`;
+			try {
+				let result = await axios.post("/graphql", {
+					query,
+				});
+
+				let signin = result.data.data.signin;
+
+				if (signin.hasOwnProperty("token")) {
+					alert("Success");
+				} else {
+					alert(signin.message);
+				}
+			} catch (err) {
+				console.log(err);
+			}
+		} else {
+			//signup
+			const query = `
+				mutation {
+					signup (email: "${email}", password: "${password}") {
+					__typename
+					... on User {
+					userId
+					}
+					
+					... on Error {
+					message
+					}
+				}
+				}
+			`;
+			try {
+				let result = await axios.post("/graphql", {
+					query,
+				});
+
+				let signup = result.data.data.signup;
+
+				if (signup.hasOwnProperty("userId")) {
+					alert("Success");
+				} else {
+					alert(signup.message);
+				}
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	};
+
 	render() {
-		return <h1>The Auth Page</h1>;
+		return (
+			<form className="auth-form" onSubmit={this.submitHandler}>
+				<div className="form-control">
+					<label htmlFor="email">E-Mail</label>
+					<input type="email" id="email" ref={this.emailEl} />
+				</div>
+				<div className="form-control">
+					<label htmlFor="password">Password</label>
+					<input type="password" id="password" ref={this.passwordEl} />
+				</div>
+				<div className="form-actions">
+					<button type="submit">Submit</button>
+					<button type="button" onClick={this.switchModeHandler}>
+						Switch to {this.state.isLogin ? "Signup" : "Login"}
+					</button>
+				</div>
+			</form>
+		);
 	}
 }
 
